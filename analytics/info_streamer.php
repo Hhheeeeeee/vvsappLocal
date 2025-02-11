2075c3765bbff982c31514c51e9c366c5661ce3c
Hhheeeeeee <149517661+Hhheeeeeee@users.noreply.github.com>
2025-02-11 16:03:21 +0100

    Update info_streamer.php

--- analytics/info_streamer.php
+++ analytics/info_streamer.php
@@ -74,7 +74,7 @@ if ($httpCode === 200) {
 
     header('Content-Type: application/json');
     http_response_code($httpCode);
-    echo "<pre>". json_encode($formattedData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)."</pre>";
+    echo json_encode($formattedData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 } else {
     http_response_code($httpCode);
     echo json_encode(["error" => "Twitch API request failed."]);
