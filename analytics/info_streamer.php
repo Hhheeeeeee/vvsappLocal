4e41a88f816e552a4d90c742c43f42263b4e24a7
Hajarr <149517661+Hhheeeeeee@users.noreply.github.com>
2025-02-13 21:10:30 +0100

    Update info_streamer.php

--- analytics/info_streamer.php
+++ analytics/info_streamer.php
@@ -57,7 +57,7 @@ if ($httpCode === 200) {
         exit;
     }
 
-    $userData = $data['data'][0];
+    $userData = $data['data'];
 
         $formattedData = [
             "id" => $userData['id'] ?? "N/A",
@@ -74,7 +74,7 @@ if ($httpCode === 200) {
 
     header('Content-Type: application/json');
     http_response_code($httpCode);
-    echo "<pre>" . json_encode($formattedData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) . "</pre>";
+    echo "<pre>" . json_encode($userData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) . "</pre>";
 } else {
     http_response_code($httpCode);
     echo json_encode(["error" => "Twitch API request failed."]);
