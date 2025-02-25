b3c0873fc898d670446f5eebfb576673f17bd90e
Hajar <hajarboukhlik123@gmail.com>
2025-02-25 17:37:53 +0100

    Añadir archivos necesarios para Heroku

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a631d690deb67b76258f294dd4a04cb559d135d8',
+        'reference' => 'ea64d806b15470778f4310b58e2d25dfb79f7f03',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a631d690deb67b76258f294dd4a04cb559d135d8',
+            'reference' => 'ea64d806b15470778f4310b58e2d25dfb79f7f03',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
