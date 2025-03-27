435c01e1597a53059b5c410521814952986c86fe
AndreaZ2306 <117767655+AndreaZ2306@users.noreply.github.com>
2025-03-27 17:48:07 +0100

    Update info_streamer.php

--- analytics/info_streamer.php
+++ analytics/info_streamer.php
@@ -30,6 +30,8 @@ if (!file_exists($tokenFile)) {
 }
 
 $tokenData = json_decode(file_get_contents($tokenFile), true);
+unset($tokenData["access_token"], $tokenData["expires_in"], $tokenData["token_type"], $tokenData["expires_at"]);
+
 if (!isset($tokenData['access_token']) || time() >= $tokenData['expires_at']) {
     http_response_code(401);
     echo json_encode(["error" => "Unauthorized. Twitch access token is invalid or has expired."]);
